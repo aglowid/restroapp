968ee2816b86113a31b694689f0f7c523f178587
amit@aglowiditsolutions.com <Amit Bhanderi>
2016-12-05 12:00:18 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Restroapp::Application.config.secret_key_base = 'aed8944e5e938f48e6b2219badacbf275f530b57d3c7f4542c607f1e1888225f31c91015d056b17c94ed8ed58e79bfcf3770dd51149b94817d64f204bc142b28'
