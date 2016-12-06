74835eaae517988b253f52f7843de21991a9063a
shefalee chaudhary <shefalee@aglowiditsolutions.com>
2016-12-06 15:41:42 +0530

    change password page added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,12 +6,18 @@ class UsersController < ApplicationController
 
 	def update
 		if params[:user][:current_password].present? && params[:user][:password].present?
-  		if @user.update_with_password(change_password_params)
-  			redirect_to super_admin_user_path ,:notice => "password successfully updated."
+  		if current_user.update_with_password(change_password_params)
+  			redirect_to change_password_users_path ,:notice => "password successfully updated."
 			else
-  			redirect_to change_password_super_admin_users_path , :notice => "Current password doesn't match."
+  			redirect_to change_password_users_path , :notice => "Current password doesn't match."
 			end	
+		else
+			redirect_to change_password_users_path , :notice => "Current password required."
 		end		
 	end	
+
+	def change_password_params
+	  params.require(:user).permit(:current_password,:password)
+	end
 
 end
